de101da83dfde7113ca04e212b19f519a9211549
Brandon Turner <bturner@gmail.com>
2015-09-28 15:28:50 -0700

    Node.js server upload script updated

--- server.js
+++ server.js
@@ -1,180 +1,50 @@
-var http = require("http");
-var url = require("url");
-var formidable = require('formidable');
-var sys = require("sys");
-var fs = require("fs");
-var util = require("util");
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util'),
+    fs   = require('fs-extra');
 
-var server = http.createServer(function(req, res) {
-    // Simple path-based request dispatcher
-    switch (url.parse(req.url).pathname) {
-        case '/':
-            display_form(req, res);
-            break;
-        case '/upload':
-            upload_new(req, res);
-            break;
-        default:
-            show_404(req, res);
-            break;
-    }
-});
-
-// Server would listen on port 8000
-server.listen(8000, function() {
-	console.log('Server now running on port 8000');
-});
-
-/*
- * Display upload form
- */
-function display_form(req, res) {
-	console.log(res);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(
-        '<form action="/upload" method="post" enctype="multipart/form-data">'+
-        '<input type="file" name="upload-file">'+
-        '<input type="submit" value="Upload">'+
-        '</form>'
-    );
-    res.end();
-}
-
-/*
- * Create multipart parser to parse given request
- */
-function parse_multipart(req) {
-    var parser = multipart.parser();
-
-    // Make parser use parsed request headers
-    parser.headers = req.headers;
-
-    // Add listeners to request, transfering data to parser
-
-    req.addListener("data", function(chunk) {
-        parser.write(chunk);
+    // create server  
+    http.createServer(function(req, res) {
+  
+   // Form uploading Process code
+   //Upload route
+   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    
+ // creates a new incoming form. 
+ var form = new formidable.IncomingForm();
+ form.uploadDir = './temp/'
+ // parse a file upload
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('Upload received :\n');
+      res.end(util.inspect({fields: fields, files: files}));
     });
-
-    req.addListener("end", function() {
-        parser.close();
+    form.on('fileBegin', function(field, file){
+        fs.rename(file.path, form.uploadDir + '/' + file.name);
+    })
+ form.on('end', function(fields, files) {
+        /* Temporary location of our uploaded file */
+        var temp_path = this.openedFiles[0].path;
+        /* The file name of the uploaded file */
+        var file_name = this.openedFiles[0].name;
+        /* Location where we want to copy the uploaded file */
+        var new_location = __dirname + '/temp/';
+        fs.copy(temp_path, new_location + file_name, function(err) {  
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("success!")
+            }
+        });
     });
-
-    return parser;
-}
-
-/*
- * Handle file upload
- */
-function upload_file(req, res) {
-    // Request body is binary
-    req.setEncoding("binary");
-
-    // Handle request as multipart
-    var stream = parse_multipart(req);
-
-    var fileName = null;
-    var fileStream = null;
-
-    // Set handler for a request part received
-    stream.onPartBegin = function(part) {
-        sys.debug("Started part, name = " + part.name + ", filename = " + part.filename);
-
-        // Construct file name
-        fileName = "./uploads/" + stream.part.filename;
-
-        // Construct stream used to write to file
-        fileStream = fs.createWriteStream(fileName);
-
-        // Add error handler
-        fileStream.addListener("error", function(err) {
-            sys.debug("Got error while writing to file '" + fileName + "': ", err);
-        });
-
-        // Add drain (all queued data written) handler to resume receiving request data
-        fileStream.addListener("drain", function() {
-            req.resume();
-        });
-    };
-
-    // Set handler for a request part body chunk received
-    stream.onData = function(chunk) {
-        // Pause receiving request data (until current chunk is written)
-        req.pause();
-
-        // Write chunk to file
-        // Note that it is important to write in binary mode
-        // Otherwise UTF-8 characters are interpreted
-        sys.debug("Writing chunk");
-        fileStream.write(chunk, "binary");
-    };
-
-    // Set handler for request completed
-    stream.onEnd = function() {
-        // As this is after request completed, all writes should have been queued by now
-        // So following callback will be executed after all the data is written out
-        fileStream.addListener("drain", function() {
-            // Close file stream
-            fileStream.end();
-            // Handle request completion, as all chunks were already written
-            upload_complete(res);
-        });
-    };
-}
-
-function upload_new(req, res) {
-	var form = new formidable.IncomingForm();
-
-	form.uploadDir = __dirname 
-	form.keepExtensions = true;
-
-	form
-    .on('error', function(err) {
-        throw err;
-    })
-
-    .on('field', function(field, value) {
-        //receive form fields here
-    })
-
-    /* this is where the renaming happens */
-    .on ('fileBegin', function(name, file){
-            //rename the incoming file to the file's name
-            file.path = form.uploadDir + "/" + file.name;
-    })
-
-    .on('file', function(field, file) {
-        //On file received
-    })
-
-    .on('progress', function(bytesReceived, bytesExpected) {
-        //self.emit('progess', bytesReceived, bytesExpected)
-
-        var percent = (bytesReceived / bytesExpected * 100) | 0;
-        process.stdout.write('Uploading: %' + percent + '\r');
-    })
-
-    .on('end', function() {
-
-
-    });
-}
-
-function upload_complete(res) {
-    sys.debug("Request complete");
-
-    // Render response
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Thanks for playing!");
-    res.end();
-
-    sys.puts("\n=> Done");
-}
-
-/*
- * Handles page not found error
- */
-function show_404(req, res) {
-    res.writeHead(404, {"Content-Type": "text/plain"});
-    res.write("You r doing it rong!");
-    res.end();
-}
+    return;
+  }
+  /* Displaying file upload form. */
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" method="post" enctype="multipart/form-data">'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(8080);
